ec4183890f83acd3c9e33ad02328e9550a4ddf12
BubblezCJ <drizzitcj@yahoo.ca>
2016-02-20 15:22:48 -0600

    Flashlight Stuff
    Flashlight Stuff

---
+++ Flashlight_Stuff/Battery.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battery : MonoBehaviour {
+
+// Controls flickering of flashlight
+	public bool isFlicker;
+// Measure of remaining battery life
+	private float battLife;
+// Controls the speed of battery diminishing
+	public float battRate;
+// Indicates if flashlight is on or not
+	public bool isOn;
+// Flicker script for flickering flashlights
+	public Light_Flicker flicker;
+// Used to affect the odds of flashlight flicker
+	private float random;
+
+
+	void Start () {
+		GetComponent<Light> ().intensity = 0.0f;
+		isFlicker = false;
+		battLife = 100.0f;
+// 10 for test, 5 or less for play
+		battRate = 10.0f;
+		isOn = false;
+	}
+
+
+
+	void Update () {
+// Hold spacebar to turn light on or off
+		if (Input.GetKey ("space") && !isFlicker && battLife > 0 && !isOn) {
+			isOn = true;
+			GetComponent<Light> ().intensity = 2.0f;
+// When light is flickering, light should not flicker
+			if (!isFlicker) {
+				random = Random.Range (1.0f, 50.0f);
+// Lower battery levels increase chance of flicker
+				if (random > battLife) { 
+					isFlicker = true;
+					flicker.Flicker ();
+				}
+			}
+		}
+// Turns light off when light is on
+		if (Input.GetKeyUp("space") && isOn) {
+			isOn = false;
+			GetComponent<Light> ().intensity = 0.0f;
+		}
+// Battery decreases when flashlight on or flickering
+		if (isOn || isFlicker) {
+			StartCoroutine(BattBurn());
+		}
+	}
+
+
+	void FixedUpdate () {
+// Battery cannot be greater than 100% or less than 0%
+//  if Battery greater than 100%, set to 100%
+		if (battLife > 100.0f) {
+			battLife = 100.0f;
+		}
+//  if Battery less than 0%, set to 0%
+		if (battLife < 0) {
+			battLife = 0;
+		}
+	}
+
+
+// BattBurn controls the diminishing battery life
+	IEnumerator BattBurn () {
+		battLife -= battRate * Time.deltaTime;
+		Debug.Log (battLife);
+		yield return new WaitForSeconds(0.1f);
+	}
+}
---
+++ Flashlight_Stuff/Light_Flicker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light_Flicker : MonoBehaviour {
+
+// Control the speed of flickering
+	private float minFlickerspeed;
+	private float maxFlickerspeed;
+// For resetting flicker possibility
+	public Battery battery;
+
+//Flickerspeed cannot be adjusted from outside
+	void Start() {
+		minFlickerspeed = 0.1f;
+		maxFlickerspeed = 0.5f;
+	}
+
+// Flicker is called to flicker the light
+	public void Flicker () {
+			StartCoroutine (Flickerspeed ());
+	}
+
+// The light flickers randomly, based on the flickerspeed
+	private IEnumerator Flickerspeed () {
+		yield return new WaitForSeconds (Random.Range(minFlickerspeed,maxFlickerspeed));
+		GetComponent<Light>().intensity = 0.0f;
+		yield return new WaitForSeconds (Random.Range(minFlickerspeed,maxFlickerspeed));
+		GetComponent<Light>().intensity = 2.0f;
+		yield return new WaitForSeconds (Random.Range(minFlickerspeed,maxFlickerspeed));
+		GetComponent<Light>().intensity = 0.0f;
+		yield return new WaitForSeconds (Random.Range(minFlickerspeed,maxFlickerspeed));
+		GetComponent<Light>().intensity = 2.0f;
+		battery.isFlicker = false;
+	}
+}
