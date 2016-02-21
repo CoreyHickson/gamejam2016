16f6680a58ac3ed793efabd4f8a6a4f0dac06ebf
BubblezCJ <drizzitcj@yahoo.ca>
2016-02-21 13:09:39 -0600

    Spawnins
    Must add jumpbot & computer prefabs via inspector

---
+++ Jumpbot_Spawn/Spoopy_Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spoopy_Spawner : MonoBehaviour {
+
+	private float rand;
+	public GameObject computer;
+	public GameObject jumper;
+
+	void Start () {
+		rand = Random.Range (0, 50);
+		if (rand > 42) {
+			Instantiate (jumper, transform.position, transform.rotation);
+		} else {
+			Instantiate (computer, transform.position, transform.rotation);
+		}
+	}
+}
