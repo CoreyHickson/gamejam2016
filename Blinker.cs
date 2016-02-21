21837c8c2bf98b033e5926a1c90098908c44fb9d
BubblezCJ <drizzitcj@yahoo.ca>
2016-02-21 13:49:27 -0600

    Blink     Blink     Blink

---
+++ Blinker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinker : MonoBehaviour {
+
+	public float minWait;
+	public float maxWait;
+
+	public float minBlinkspeed;
+	public float maxBlinkspeed;
+
+	void Start (){
+		StartCoroutine (Blink ());
+	}
+
+	private IEnumerator Blink () {
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (Random.Range(minBlinkspeed, maxBlinkspeed));
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (Random.Range(minWait, maxWait));
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (Random.Range(minBlinkspeed, maxBlinkspeed));
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (Random.Range(minWait, maxWait));
+		StartCoroutine (Blink ());
+	}
+}
